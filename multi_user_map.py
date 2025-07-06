fb52a53f5e59db7f4a90ff75ace10bcb445b2215
Adonis Hautea <donhautea@gmail.com>
2025-07-06 22:20:28 +0800

    main

--- multi_user_map.py
+++ multi_user_map.py
@@ -184,7 +184,7 @@ if show_path and path_user:
             data=df_user_path,
             get_position="[lon, lat]",
             get_fill_color="Color",
-            get_radius=40,
+            get_radius=20,
             pickable=True
         )
         text = pdk.Layer(
